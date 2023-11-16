5165ac96b8e2b1382754c3c9151b1629e9f29c48
mrxdoffical <berogergesg@gmail.com>
2023-11-16 08:48:21 +0200

    added tasks

--- string_handler.c
+++ string_handler.c
@@ -1,41 +1,41 @@
 #include"main.h"
 
 /**
- * string_handler - an function that can handle strings
- * @lists: variadic list holds int
- * Return: len
-*/
+ * string_handler - a function that handles strings
+ * @lists: variadic list that holds the string
+ * Return: length of the string after handling
+ */
 int string_handler(va_list lists)
 {
 	char *string;
-	int i, len = 0;
+	int len = 0;
 	int value;
 
 	string = va_arg(lists, char *);
 	if (string == NULL)
-	{
 		string = "(NULL)";
-	}
-	for (i = 0; string[i] != '\0'; i++)
+
+	for (; *string != '\0'; string++)
 	{
-		if (string[1] < 32 || string[i] >= 127)
+		if (*string < 32 || *string >= 127)
+		{
+			_putchar('\\');
+			_putchar('x');
+			len += 2;
+			value = *string;
+			if (value < 16)
 			{
-				_putchar('\\');
-				_putchar('x');
-				len = len + 2;
-				value = string[i];
-				if (value < 16)
-				{
-					_putchar('0');
-					len++;
-				}
-				len = len + upper_hex_handler(value);
+				_putchar('0');
+				len++;
 			}
+			len += upper_hex_handler(value);
+		}
 		else
 		{
-			_putchar(string[i]);
+			_putchar(*string);
 			len++;
 		}
 	}
+
 	return (len);
 }
