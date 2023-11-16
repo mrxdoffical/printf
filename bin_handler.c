36c891e54a9aae42327410e346a202364eed2c54
mrxdoffical <berogergesg@gmail.com>
2023-11-16 08:59:27 +0200

    added tasks

--- bin_handler.c
+++ bin_handler.c
@@ -1,38 +1,44 @@
-#include "main.h"
-
+#include"main.h"
 /**
- * bin_handler - handles unsigned integer to binary conversion
+ * bin_handler - handles unsignd integer to binary
  * @lists: variadic list
- * Return: number of digits in the binary representation
- */
+ * Return: intger of counter
+*/
+
 int bin_handler(va_list lists)
 {
 	int sign = 0;
+
 	int counter = 0;
+
+	int i;
+
+	int b;
+
+	int bit_mask = 1;
+
+	unsigned int storage;
+
 	unsigned int num = va_arg(lists, unsigned int);
 
-	for (int i = 31; i >= 0; i--)
+	for (i = 0; i < 32 ; i++)
 	{
-		unsigned int bit_mask = 1 << i;
-		unsigned int storage = bit_mask & num;
-
-		if (storage)
-			sign = 1;
-
+		storage = ((bit_mask << (32 - i)) & num);
+		if (storage >> (32 - i))
+		sign = 1;
 		if (sign)
 		{
-			int b = storage >> i;
-
-			_putchar(b + '0');
+			b = storage >> (32 - i);
+			_putchar(b + 48);
 			counter++;
 		}
 	}
-
 	if (counter == 0)
 	{
 		counter++;
 		_putchar('0');
 	}
-
 	return (counter);
+
+
 }
