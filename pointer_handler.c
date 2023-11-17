5ff4379fd9931e997f83dcc64b5cb072fc10c165
mrxdoffical <berogergesg@gmail.com>
2023-11-17 15:38:05 +0200

    added tasks

--- pointer_handler.c
+++ pointer_handler.c
@@ -1,19 +1,22 @@
 #include"main.h"
 /**
- * pointer_handler - handle pointers
- * @lists: variadic lists
- * Return: returns placeholders
+ * print_pointers - handle pointers
+ * @count: variadic lists
 */
 
 void print_pointers(int count, ...)
 {
-    va_list args;
-    va_start(args, count);
+	va_list args;
 
-    for (int i = 0; i < count; ++i) {
-        void *ptr = va_arg(args, void *);
-        printf("%p\n", ptr);
-    }
+	va_start(args, count);
 
-    va_end(args);
+
+	for (int i = 0; i < count; ++i)
+	{
+		void *ptr = va_arg(args, void *);
+
+		printf("%p\n", ptr);
+	}
+
+	va_end(args);
 }
